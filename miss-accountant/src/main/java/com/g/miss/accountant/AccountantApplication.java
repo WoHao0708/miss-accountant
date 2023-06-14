232a9a2073a9f31ae0cf90c9a274ed78149f3902
howard <a6011034@gmail.com>
2023-06-14 17:49:52 +0800

    -Add config folder.

--- miss-accountant/src/main/java/com/g/miss/accountant/AccountantApplication.java
+++ miss-accountant/src/main/java/com/g/miss/accountant/AccountantApplication.java
@@ -21,16 +21,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @SpringBootApplication
 @MapperScan("com.g.miss.accountant.dao")
 public class AccountantApplication {
-    static Path downloadedContentDir;
 
     public static void main(String[] args) throws IOException {
-        downloadedContentDir = Files.createTempDirectory("line-bot");
         SpringApplication.run(AccountantApplication.class, args);
     }
 
--- miss-accountant/src/main/java/com/g/miss/accountant/config/AccountantWebMvcConfigurer.java
+++ miss-accountant/src/main/java/com/g/miss/accountant/config/AccountantWebMvcConfigurer.java
@@ -14,22 +14,30 @@
  * under the License.
  */
 
-package com.g.miss.accountant;
+package com.g.miss.accountant.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Configuration
 @Slf4j
 public class AccountantWebMvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Path downloadedContentDir = null;
+        try {
+            downloadedContentDir = Files.createTempDirectory("line-bot");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        String downloadedContentUri = AccountantApplication.downloadedContentDir
-                .toUri().toASCIIString();
+        String downloadedContentUri = downloadedContentDir.toUri().toASCIIString();
         log.info("downloaded dir: {}", downloadedContentUri);
         registry.addResourceHandler("/downloaded/**")
                 .addResourceLocations(downloadedContentUri);
