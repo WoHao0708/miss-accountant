cb5454c4bf3841b787ded9c9e9c9ceb15dd21a09
howard <a6011034@gmail.com>
2023-06-19 18:04:30 +0800

    -Adjust openAPI.

--- miss-accountant/src/main/java/com/g/miss/accountant/controller/AccountController.java
+++ miss-accountant/src/main/java/com/g/miss/accountant/controller/AccountController.java
@@ -6,6 +6,8 @@ import com.g.miss.accountant.util.ValidationUtils;
 import com.g.miss.accountant.vo.AccountVO;
 import com.g.miss.accountant.vo.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,6 +36,11 @@ public class AccountController {
      * @param name    名稱
      * @return 成功或失敗
      */
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "groupId", value = "群組ID", dataType = "string", required = true),
+            @ApiImplicitParam(name = "userId", value = "債權人ID", dataType = "string", required = true),
+            @ApiImplicitParam(name = "name", value = "筆記", dataType = "string", required = true),
+    })
     @PostMapping("/account/update")
     @ApiOperation(value = "更新使用者資訊")
     public Result<?> update(String groupId, String userId, String name) {
@@ -50,6 +57,11 @@ public class AccountController {
      * @param name    名稱
      * @return 群組內帳號
      */
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "groupId", value = "群組ID", dataType = "string", required = true),
+            @ApiImplicitParam(name = "userId", value = "債權人ID", dataType = "string", required = true),
+            @ApiImplicitParam(name = "name", value = "筆記", dataType = "string", required = true),
+    })
     @GetMapping("/account/group")
     @ApiOperation(value = "取得群組內的使用者")
     public Result<List<AccountDTO>> group(String groupId, String userId, String name) {
--- miss-accountant/src/main/java/com/g/miss/accountant/controller/DebtController.java
+++ miss-accountant/src/main/java/com/g/miss/accountant/controller/DebtController.java
@@ -6,6 +6,8 @@ import com.g.miss.accountant.util.ValidationUtils;
 import com.g.miss.accountant.vo.DebtVO;
 import com.g.miss.accountant.vo.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -33,6 +35,14 @@ public class DebtController {
      * @param note    筆記
      * @return 結果
      */
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "groupId", value = "群組ID", dataType = "string", required = true),
+            @ApiImplicitParam(name = "userId", value = "債權人ID", dataType = "string", required = true),
+            @ApiImplicitParam(name = "userIds", value = "負債人IDs", dataType = "string", required = true, allowMultiple = true),
+            @ApiImplicitParam(name = "amount", value = "金額", dataType = "int", required = true),
+            @ApiImplicitParam(name = "note", value = "筆記", dataType = "string", required = true),
+    })
     @PostMapping("/debt/add")
     @ApiOperation(value = "記帳")
     public Result<?> addDebt(String groupId, String userId, String[] userIds, Integer amount, String note) {
@@ -48,9 +58,13 @@ public class DebtController {
      * @param groupId 群組id
      * @return 與使用者相關的債權
      */
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "groupId", value = "群組ID", dataType = "string", required = true),
+            @ApiImplicitParam(name = "userId", value = "使用者ID", dataType = "string", required = true),
+    })
     @GetMapping("/debt/list")
     @ApiOperation(value = "取得與使用者相關的債權")
-    public Result<List<DebtDTO>> getList(@RequestParam String userId, @RequestParam String groupId) {
+    public Result<List<DebtDTO>> getList(@RequestParam String groupId, @RequestParam String userId) {
         return Result.ok(debtService.listDebt(groupId, userId));
     }
 
@@ -60,6 +74,7 @@ public class DebtController {
      * @param debtId 債權id
      * @return 結果
      */
+    @ApiImplicitParam(name = "debtId", value = "債權ID", dataType = "string", required = true)
     @DeleteMapping("/debt/delete")
     @ApiOperation(value = "刪除單筆債權")
     public Result<?> deleteDebt(@RequestParam String debtId) {
